4a6b5cf01d409dba94c07438d04d239c229cfeff
arjesh <arjeshvadadoriya+github@gmail.com>
2024-08-27 17:51:16 +0530

    pattern

---
+++ patterns/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	printPattern()
+	//printPatternRec(0, 4)
+}
+
+func printPattern() {
+	rows := 4
+	for r := 0; r < rows; r++ {
+		for s := 0; s < rows-(r+1); s++ {
+			fmt.Print(" ")
+		}
+		for c := 0; c < r+1; c++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
+
+func space(i int, max int) {
+	if i > max {
+		fmt.Println()
+		return
+	}
+	if i > 0 {
+		fmt.Print(" ")
+	}
+	space(i+1, max)
+}
+
+func printPatternRec(i int, max int) {
+	if i > max {
+		fmt.Println()
+		return
+	}
+	space(0, i+1)
+	fmt.Print("*")
+	printPatternRec(i+1, max)
+}
